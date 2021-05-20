00ff55e502610f760ef6f96bc0cb0ab9fcb633ed
mdsohelranaahc793 <84516385+mdsohelranaahc793@users.noreply.github.com>
2021-05-20 20:34:39 +0600

    my-portfolio

---
+++ main.js
@@ -0,0 +1,105 @@
+
+$(document).ready(function(){
+
+ 
+ /*
+ 
+  $('.js--service-section').waypoint(function(direction){
+    if(direction=="down"){
+      $('nav').addClass('sticky');
+    }
+    else{
+         $('nav').removeClass('sticky');
+    }
+  });
+  */
+ 
+ 
+ 
+  $('.web,.icons,.app').show();
+
+  $('#web').click(function(){
+    $('.web').show();
+  });
+  $('#apps').on('click', function() {
+    $('.app').show();
+  }, function(){
+    $('.web,.icons').hide();
+  });
+  $('#icons').click(function() {
+    $('.icons').show();
+  });
+  
+ 
+  
+  
+  
+  
+  
+
+$('svg.radial-progress').each(function( index, value ) {
+
+  $(this).find($('circle.complete')).removeAttr('style' );
+
+});
+
+
+
+$(window).scroll(function(){
+
+  $('svg.radial-progress').each(function( index, value ) {
+
+    // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+
+    if (
+
+        $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+
+        $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+
+    ) {
+
+        // Get percentage of progress
+
+        percent = $(value).data('percentage');
+
+        // Get radius of the svg's circle.complete
+
+        radius = $(this).find($('circle.complete')).attr('r');
+
+        // Get circumference (2πr)
+
+        circumference = 2 * Math.PI * radius;
+
+        // Get stroke-dashoffset value based on the percentage of the circumference
+
+        strokeDashOffset = circumference - ((percent * circumference) / 100);
+
+        // Transition progress for 1.25 seconds
+
+        $(this).find($('circle.complete')).animate({'stroke-dashoffset': strokeDashOffset}, 1250);
+
+    }
+
+  });
+
+}).trigger('scroll');
+
+alert('Hello! Wellcome to my web template.');
+ 
+});
+
+function thanks() {
+  var fill = $('.name-field').val();
+  var fill2 = $('.text-area-field').val();
+  if (fill != "" && fill2 != "") {
+    alert('Thank you for messeging me.');
+  }
+  else {
+    alert('Fill the required first');
+  }
+}
+
+function demo() {
+  alert('This is a demo, so link is not working right not!')
+}
